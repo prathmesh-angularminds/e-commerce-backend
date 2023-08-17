f79c1213fbfd853eb37db40b09d3d5cf0a6304e5
prathmesh-angularminds <prathmesh.vibhute@angularminds.in>
2023-08-17 18:26:22 +0530

    Multer working

--- src/app.js
+++ src/app.js
@@ -6,17 +6,18 @@ const cors = require('cors')
 const multer = require('multer');
 const httpStatus = require('http-status')
 const catchAsync = require('./utils/catchAsync');
-
 const router = require('./routes/index')
 const upload = multer({ dest: "images/" })
+
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json())
-// app.use(multer({dest: 'images/'}).single('image'))
 
 // console.log("Multer: ",) 
 // app.use('/api',router)
 app.use('/api',upload.single('image'),(req,res,next) => {
-    console.log(req.file);
+    console.log("Headers: ",req.headers);
+    console.log("File: ",req.file);
+    console.log("Body: ",req.body.file);
 })
 
 app.use(catchAsync((req,res,next) => {
